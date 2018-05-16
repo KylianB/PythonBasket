d3af6505464e22223274cf20ee836678ea1885c2
KylianB <kylian.balde@gmail.com>
2018-05-16 19:04:19 +0200

    Updtate de la ligne de preview BUG de lancement a revoir

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from pygame.locals import *
 import pygame
 import math
 import random
-pygame.init()
 
+pygame.init()
 
 GREEN = (20, 255, 140)
 GREY = (210, 210, 210)
@@ -17,6 +18,10 @@ SCREENHEIGHT = 720
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Basket Game")
+
+pts_list = []
+
+vitesse = [10, 45]
 
 
 def print_text(text, x, y, fontsize, color):
@@ -44,6 +49,7 @@ def draw_life_bar(surf, x, y, life):
     pygame.draw.rect(surf, GREEN, fill_rect)
     pygame.draw.rect(surf, WHITE, outline_rect, 2)
 
+
 # Classes
 
 
@@ -58,17 +64,34 @@ class Ball(pygame.sprite.Sprite):
         self.position = [10, 600]
         self.rect.x = self.position[0]
         self.rect.y = self.position[1]
-        self.last_pos = (self.position[0], self.position[1])
-
 
     def update(self):
         global start
-        self.collide()
+        # self.collide()
+        self.preview()
         if start:
             self.evolution()
 
+    def preview(self):
+        global alphaRad, pts_list
+        pts_list = [[self.position[0], self.position[1]]]
+        vitesse_prev = [math.cos(alphaRad) * force, -math.sin(alphaRad) * force]
+        for i in range(20):
+            pos = [0, 0]
+            self.position[0] = pos[0]
+            self.position[1] = pos[1]
+
+            pos[0] += vitesse_prev[0]
+            pos[1] += vitesse_prev[1]
+
+            vitesse_prev[1] += 4
+            vitesse_prev[0] *= 0.99
+            vitesse_prev[1] *= 0.99
+
+            pts_list += [[pos[0], pos[1]]]
+
     def evolution(self):
-        global vitesse
+        global vitesse, start
         self.position[0] += vitesse[0]
         self.position[1] += vitesse[1]
 
@@ -82,19 +105,52 @@ class Ball(pygame.sprite.Sprite):
         global vitesse, force
         ball_collide = pygame.sprite.spritecollide(self, collide_group, False)
         if ball_collide:
-            CoeffRebond = 0.7
-            #vitesse = [int(math.cos(alphaRad) * force) * CoeffRebond, int(-math.sin(alphaRad) * force) * CoeffRebond]
-            vitesse = [int(vitesse[0]*CoeffRebond),-int(vitesse[1]*CoeffRebond)]
+            coeff_rebondy = 0.9
+            coeff_rebondx = 0.9
+            vitesse = [vitesse[0]*coeff_rebondy, -vitesse[1]*coeff_rebondx]
+            force = force * 0.9
+            vitesse = [math.cos(alphaRad) * force, -math.sin(alphaRad) * force]
+
 
 class Panier(pygame.sprite.Sprite):
-
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("images/panier_basket.png")
-
+        self.image = pygame.image.load("images/panier_basket_base.png")
         self.rect = self.image.get_rect()
-        self.rect.x = 1000
-        self.rect.y = 100
+        self.rect.x = 1100
+        self.rect.y = 250
+
+
+class Panier2(Panier):
+    def __init__(self):
+        Panier.__init__(self)
+        self.image = pygame.image.load("images/panier_basket_base2.png")
+        self.rect.x = 1060
+        self.rect.y = 255
+
+
+class Panier3(Panier):
+    def __init__(self):
+        Panier.__init__(self)
+        self.image = pygame.image.load("images/panier_basket_base3.png")
+        self.rect.x = 1019
+        self.rect.y = 255
+
+
+class Panier4(Panier):
+    def __init__(self):
+        Panier.__init__(self)
+        self.image = pygame.image.load("images/panier_basket_base4.png")
+        self.rect.x = 1009
+        self.rect.y = 255
+
+
+class Panier5(Panier):
+    def __init__(self):
+        Panier.__init__(self)
+        self.image = pygame.image.load("images/panier_basket_pannel.png")
+        self.rect.x = 1094
+        self.rect.y = 149
 
 
 class Sol(pygame.sprite.Sprite):
@@ -109,6 +165,10 @@ class Sol(pygame.sprite.Sprite):
 # References aux classes
 ball = Ball()
 panier = Panier()
+panier2 = Panier2()
+panier3 = Panier3()
+panier4 = Panier4()
+panier5 = Panier5()
 sol = Sol()
 
 # Les sprites du jeu
@@ -116,21 +176,25 @@ balls_group = pygame.sprite.Group()
 balls_group.add(ball)
 
 collide_group = pygame.sprite.Group()
-collide_group.add(sol, panier)
+collide_group.add(sol, panier, panier2, panier4, panier5)
+
+collide_score = pygame.sprite.Group()
+collide_score.add(panier3)
 
 all_sprite_group = pygame.sprite.Group()
-all_sprite_group.add(balls_group, collide_group)
+all_sprite_group.add(balls_group, collide_group, collide_score)
 
 force = 10
 alpha = 45
+alphaRad = math.radians(alpha)
 
 ###
 start = False
-
+show_line = True
 
 playing = True
 clock = pygame.time.Clock()
-vitesse = 0
+# vitesse = 0
 
 while playing:
     for event in pygame.event.get():
@@ -138,12 +202,19 @@ while playing:
             playing = False
         elif event.type == pygame.KEYDOWN:
 
-            if event.key == pygame.K_x:  # X pour fermer le jeu
+            if event.key == pygame.K_ESCAPE:
                 playing = False
             if event.key == pygame.K_SPACE:
-                alphaRad = math.radians(alpha)
-                vitesse = [math.cos(alphaRad) * force, -math.sin(alphaRad) * force]
+                # vitesse = [math.cos(alphaRad) * force, -math.sin(alphaRad) * force]
                 start = True
+                show_line = False
+
+            if event.key == pygame.K_k:
+                start = False
+                show_line = True
+                ball.rect.x = 10
+                ball.rect.y = 600
+                ball.position = [ball.rect.x, ball.rect.y]
 
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
@@ -169,9 +240,10 @@ while playing:
     # Couleurs fond
     screen.fill(BLACK)
     all_sprite_group.draw(screen)
-    print_text(str(force), 900, 10, 20, WHITE)
-    print_text(str(alpha), 900, 30, 20, WHITE)
-    print_text(str(vitesse), 900, 50, 20, WHITE)
+    print_text(str(format(force, '.2f')), 900, 10, 20, WHITE)
+    print_text(str(format(alpha, '.2f')), 900, 30, 20, WHITE)
+    if show_line:
+        pygame.draw.lines(screen, WHITE, False, pts_list)
 
     # Refresh
     pygame.display.flip()
@@ -179,7 +251,4 @@ while playing:
     # 60FPS
     clock.tick(60)
 
-
 pygame.quit()
-
-
